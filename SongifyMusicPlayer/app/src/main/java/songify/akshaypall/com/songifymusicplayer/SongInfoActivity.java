b194a9a0d6dc24418ec4a2bc7b8bc2c062e8d4b7
AkshayPall <iosakshay@gmail.com>
2017-06-23 16:21:49 -0400

    textview shows parsed song data now

--- SongifyMusicPlayer/app/src/main/java/songify/akshaypall/com/songifymusicplayer/SongInfoActivity.java
+++ SongifyMusicPlayer/app/src/main/java/songify/akshaypall/com/songifymusicplayer/SongInfoActivity.java
@@ -4,13 +4,19 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import songify.akshaypall.com.songifymusicplayer.Utils.BlurBackground;
 
 public class SongInfoActivity extends Activity {
 
+    // Tags and Bundle Keys
     private static final String TAG = SongInfoActivity.class.getSimpleName();
     public static final String KEY_INFO_STRING = "KEY_INFO_STRING";
+
+    // Fields to display data in buncle
+    private String mData;
+    private TextView mSongDataTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,9 @@ public class SongInfoActivity extends Activity {
 
         Bitmap background = getIntent().getExtras().getParcelable(BlurBackground.KEY_BACKGROUND_BITMAP);
         getWindow().setBackgroundDrawable(new BitmapDrawable(getResources(), background));
+
+        mSongDataTv = (TextView) findViewById(R.id.song_info_tv);
+        mData = getIntent().getExtras().getString(KEY_INFO_STRING, "");
+        mSongDataTv.setText(mData);
     }
 }
